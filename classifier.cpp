60e2c58b260d84f1ff1ba6f92bfb61c66c3ce85c
Shawaqfeh715 <Anasshaw@umich.edu>
2026-03-13 22:33:19 -0400

    set up
    set up of classifier.exe, classifier.cpp, and debugger

--- classifier.cpp
+++ classifier.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
+#include "csvstream.hpp"
 using namespace std;
 
-int main(){
-   cout<<"Hello world!";
+// EFFECTS: Return a set of unique whitespace delimited words
+set<string> unique_words(const string &str) {
+  istringstream source(str);
+  set<string> words;
+  string word;
+  while (source >> word) {
+    words.insert(word);
+  }
+  return words;
 }
