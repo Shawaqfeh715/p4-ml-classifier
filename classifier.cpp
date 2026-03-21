adb850e223e2d4ba8bcd7967c2eb3d6c02804e89
Shawaqfeh715 <Anasshaw@umich.edu>
2026-03-21 17:53:36 -0400

    reading files
    implemented the main opening and reading the files in classifier.cpp

--- classifier.cpp
+++ classifier.cpp
@@ -2,22 +2,66 @@
 #include <string>
 #include "csvstream.hpp"
 #include <cassert>
+#include <cmath>
+#include <string>
+#include <map>
 using namespace std;
 
 
+int main(int argc,char* argv[]){
 
-//returns number of posts
+    // check number of arguments
+    if (argc!=2 && argc!=3)
+    {
+      cout << "Usage: classifier.exe TRAIN_FILE [TEST_FILE]" << endl;
+    }
+
+    //open and process training file
+
+    string train_name=argv[1];
+
+    try
+    {
+      csvstream train_csv(train_name);
+      map<string,string> row;
+
+      while (train_csv>>row)
+      {
+        string label=row["tag"];
+        string content=row["content"];
+      }
+      
+    }
+    catch(const csvstream_exception & e)
+    {
+      cout<<"Error opening file:"<<train_name<<endl;
+      return 1;
+    }
+
+    if (argc==3)
+    {
+      string test_name=argv[2];
+      try
+      {
+        csvstream test_csv=test_name;
+        map<string,string> row;
+
+        while (test_csv>>row)
+        {
+          string correct_label=row["tag"];
+          string content=row["content"];
+        }
+      }
+      catch(const csvstream_exception& e)
+      {
+        cout<<"Error opening file:"<<test_name<<endl;
+      }
+      
+    }
+    
+    
 
 
-// EFFECTS: Return a set of unique whitespace delimited words
-set<string> unique_words(const string &str) {
-  istringstream source(str);
-  set<string> words;
-  string word;
-  while (source >> word) {
-    words.insert(word);
-  }
-  return words;
 }
 
 
